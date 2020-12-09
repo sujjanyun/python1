a1f806976d3aabff2763cb7e156a5825f95c526a
Susan Yun <susanhkyun@gmail.com>
2020-12-09 13:47:54 -0600

    day 3

---
+++ caphello.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ")
+print(f"HELLO, {name.upper()}!")
+print(f"YOUR NAME HAS {len(name)} LETTERS IN IT! AWESOME!")
---
+++ madlib.py
@@ -0,0 +1,6 @@
+print("Please fill in the blanks below: ")
+print("__(name)__'s favorite subject in school is __(subject)__")
+name = input("What is your name? ")
+subject = input("What is subject? ")
+
+print(f"{name}'s favorite subject in school is {subject}.")
